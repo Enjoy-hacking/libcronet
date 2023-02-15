5b7d77fd057622fe0bc6ee33e924d6109287e6ee
Binary Craft <elliot.huang.signed@gmail.com>
2023-02-15 12:37:54 +0800

    ci

--- .github/actions/pkgbuild/bootstrap.sh
+++ .github/actions/pkgbuild/bootstrap.sh
@@ -8,9 +8,11 @@ echo 'nobody ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers
 git clone https://aur.archlinux.org/yay.git
 pacman -S go --noconfirm
 chown -R nobody yay
+chown -R nobody /.cache
 pushd yay
 sudo -u nobody makepkg -cri
 popd
 pacman -Rsn go --noconfirm
 pacman -Sc --noconfirm
 rm -rf yay
+rm -rf /.cache/go-build
